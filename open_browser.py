ccb7a7a2c003ba51155f04486d8277bd483bdb57
Claudio Cabaleyro <claudio_cabaleyro@cargill.com>
2026-04-29 15:23:56 -0300

    Refactor browser opening logic to use Selenium WebDriver

--- open_browser.py
+++ open_browser.py
@@ -1,9 +1,18 @@
-import webbrowser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
-URL = input("Enter a URL to open: ")
 
-def open_page(url):
-    webbrowser.open(url)
+URL = "https://cargillonline.sharepoint.com/sites/CASCSA_APU_brm/A_Stocks/Forms/last_pdf_file_only.aspx"
+
+
+def open_page(url: str) -> None:
+    options = Options()
+    options.add_experimental_option("detach", True)  # keep browser open
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
 
 if __name__ == "__main__":
     open_page(URL)
